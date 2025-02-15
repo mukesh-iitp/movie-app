2554bf5e9e54bf2c1808218776c87d7728d7c5a1
Mukesh Kumar <ermkit@gmail.com>
2025-02-15 23:01:35 +0530

    Creating basic structure of React App for movie-app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+/**Functional Components */
+// function App() {
+//   return (
+//     <h1>Functional Component</h1>
+//   );
+// }
+// export default App;
+
+/**Class Component */
+class App extends React.Component{
+
+  render(){
+    return <h1>Class Component</h1>
+  }
+
+}
+
+export default App;
