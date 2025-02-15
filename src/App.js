a5832ffc917549bf677be07f00f208ba0a360ad4
Mukesh Kumar <ermkit@gmail.com>
2025-02-15 23:50:41 +0530

    completing basic structure of app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React from "react";
+import MoiveCard from "./Moviecard";
 
-/**Functional Components */
-// function App() {
-//   return (
-//     <h1>Functional Component</h1>
-//   );
-// }
-// export default App;
-
-/**Class Component */
-class App extends React.Component{
-
-  render(){
-    return <h1>Class Component</h1>
-  }
-
+function App() {
+  return (
+    <>
+    <h1>Movie App</h1>
+    <MoiveCard/>
+    </>
+  );
 }
-
 export default App;
---
+++ src/Moviecard.js
@@ -0,0 +1,11 @@
+//import React from "react";
+import { Component } from "react";
+
+//class MoiveCard extends React.Component{
+class MoiveCard extends Component{
+    render(){
+        return <h2>Moive Card</h2>
+    }
+}
+
+export default MoiveCard;
