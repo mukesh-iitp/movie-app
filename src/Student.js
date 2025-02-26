636a8056bedeb2a57840b32c1a39317693a3b7f7
Mukesh Kumar <ermkit@gmail.com>
2025-02-26 18:17:45 +0530

    understading porps usage with functional component

--- src/Student.js
+++ src/Student.js
@@ -1,25 +1,46 @@
 import { Component } from "react";
 
-class Student extends Component{
+/**props in functional components */
 
-    render(){
+function Student(props){
 
-        console.log(this.props);
+    const {sname, marks} = props;
 
-        const {sname, marks} = this.props;
-
-        return(
+    return(
             <>
-                {/* <h3>Hello, {this.props.sname}</h3>
-                <h5>You have secured {this.props.marks} %</h5> */}
-                <h3>Hello, {sname}</h3>
-                <h5>You have secured {marks} %</h5>
-                <hr/>
+            <h3>Hello, {sname}</h3>
+            <h5>You have secured {marks} %</h5>
+            <hr/>
             </>
-        )
-
-    }
+    )
 
 }
+
+
+/**props in class components */
+
+// class Student extends Component{
+
+//     render(){
+
+//         console.log(this.props);
+
+//         const {sname, marks} = this.props;
+
+//         return(
+//             <>
+//                 {/* <h3>Hello, {this.props.sname}</h3>
+//                 <h5>You have secured {this.props.marks} %</h5> */}
+//                 <h3>Hello, {sname}</h3>
+//                 <h5>You have secured {marks} %</h5>
+//                 <hr/>
+//             </>
+//         )
+
+//     }
+
+// }
+
+
 
 export default Student;
