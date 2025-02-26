4245316b6433b7720843bc8dcd3fbcc5ccf2a689
Mukesh Kumar <ermkit@gmail.com>
2025-02-26 18:07:35 +0530

    destructing the constants

--- src/Student.js
+++ src/Student.js
@@ -5,10 +5,15 @@ class Student extends Component{
     render(){
 
         console.log(this.props);
+
+        const {sname, marks} = this.props;
+
         return(
             <>
-                <h3>Hello, {this.props.sname}</h3>
-                <h5>You have secured {this.props.marks} %</h5>
+                {/* <h3>Hello, {this.props.sname}</h3>
+                <h5>You have secured {this.props.marks} %</h5> */}
+                <h3>Hello, {sname}</h3>
+                <h5>You have secured {marks} %</h5>
                 <hr/>
             </>
         )
