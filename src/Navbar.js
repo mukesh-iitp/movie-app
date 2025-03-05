6945e97a6531ccf7c1782cf0b58d943f8aedf79a
Mukesh Kumar <ermkit@gmail.com>
2025-03-05 09:20:21 +0530

    add an inline style using react

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,8 +6,15 @@ class Navbar extends React.Component{
 
         return(
             <>
-            <div>
-                <div>Title</div>
+            {/* <div className="nav"> */}
+            <div style={{width:"100%", 
+                        height:70, 
+                        background:"cadetblue", 
+                        display:"flex",
+                        justifyContent: "space-between" }}>
+                <div className="title">
+                    Movie-App
+                </div>
                 <div>
                     <img alt="Cart Icon"/>
                     <span>0</span>
