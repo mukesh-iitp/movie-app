803529a50a8d7bb5ceaa52ef5049899e2df075d0
Mukesh Kumar <ermkit@gmail.com>
2025-03-23 17:11:55 +0530

    convertig App.js to class component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
+import React from "react";
 import HeadingOne from "./HeadingOne";
 import HeadingTwo from "./HeadingTwo";
 import MoiveList from "./MovieList";
 import Navbar from "./NavbarModule";
 //import Student from "./Student";
 
-function App() {
-  return (
-    <>
-    <Navbar/>
-    <MoiveList/>
+class App extends React.Component{
 
+  render(){
 
-    {/* <h1>Students Data</h1>
-    <Student sname="Alexa" marks="80"/>
-    <Student sname="Siri" marks="90"/>
-    <Student sname="Google" marks="88"/>
-    <Student/> */}
-    
-    
-    {/* <HeadingOne/>
-    <HeadingTwo/> */}
-    
-    </>
-  );
+    return(
+      <>
+        <Navbar/>
+        <MoiveList/>
+      </>
+    )
+
+  }
+
 }
+
+// function App() {
+//   return (
+//     <>
+//     <Navbar/>
+//     <MoiveList/>
+
+
+//     {/* <h1>Students Data</h1>
+//     <Student sname="Alexa" marks="80"/>
+//     <Student sname="Siri" marks="90"/>
+//     <Student sname="Google" marks="88"/>
+//     <Student/> */}
+    
+    
+//     {/* <HeadingOne/>
+//     <HeadingTwo/> */}
+    
+//     </>
+//   );
+// }
 
 // Student.defaultProps= {
 //   sname : "Student",
