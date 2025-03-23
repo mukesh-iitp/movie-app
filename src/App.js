d90f2a4bb7b1c3101eb9585afb8817fdebb252d6
Mukesh Kumar <ermkit@gmail.com>
2025-03-23 17:32:23 +0530

    lifting state from movielist to app.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,85 @@ import HeadingTwo from "./HeadingTwo";
 import MoiveList from "./MovieList";
 import Navbar from "./NavbarModule";
 //import Student from "./Student";
+import {movies} from "./moviesData";
 
 class App extends React.Component{
 
+  constructor(){
+    super();
+    this.state = {
+        // title: "The Avengers",
+        // plot: "The supernatural power shows",
+        // price: 199,
+        // rating: 8.9,
+        // stars: 0,
+        // fav: false,
+        // cart: false
+
+        movies : movies,
+        cartCount: 0
+
+    }
+    // this.addStars = this.addStars.bind(this);
+}
+
+handleIncStar = (movie) =>{
+    const{movies} = this.state;
+    const movieId = movies.indexOf(movie);
+    if(movies[movieId].star >=5)
+        return;
+    movies[movieId].star +=0.5;
+    this.setState({
+        //movies: movies
+        movies  //alternative or shortcut or above line for same movies
+    })
+}
+
+handleDecStar = (movie) =>{
+    const{movies} = this.state;
+    const movieId = movies.indexOf(movie);
+    if(movies[movieId].star <=0)
+        return;
+    movies[movieId].star -=0.5;
+    this.setState({
+        //movies: movies
+        movies  //alternative or shortcut or above line for same movies
+    })
+}
+handleFav = (movie) =>{
+    const{movies} = this.state;
+    const movieId = movies.indexOf(movie);
+    movies[movieId].fav = !movies[movieId].fav;
+    this.setState({
+        //movies: movies
+        movies  //alternative or shortcut or above line for same movies
+    })
+}
+handleCart = (movie) =>{
+    const{movies} = this.state;
+    const movieId = movies.indexOf(movie);
+    movies[movieId].isInCart = !movies[movieId].isInCart;
+    this.setState({
+        //movies: movies
+        movies  //alternative or shortcut or above line for same movies
+    })
+}
+
   render(){
+
+    const {movies} = this.state;
 
     return(
       <>
         <Navbar/>
-        <MoiveList/>
+        <MoiveList 
+          movies={movies}
+          key={movies.id} 
+          addStars={this.handleIncStar} 
+          removeStars={this.handleDecStar}
+          toggleFav={this.handleFav}
+          toggleCart={this.handleCart}
+        />
       </>
     )
 
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,73 +1,13 @@
 import { Component } from "react";
 import MoiveCard from "./Moviecard";
-import {movies} from "./moviesData";
+
 
 class MoiveList extends Component{
-
-    constructor(){
-        super();
-        this.state = {
-            // title: "The Avengers",
-            // plot: "The supernatural power shows",
-            // price: 199,
-            // rating: 8.9,
-            // stars: 0,
-            // fav: false,
-            // cart: false
-
-            movies : movies
-
-        }
-        // this.addStars = this.addStars.bind(this);
-    }
-
-    handleIncStar = (movie) =>{
-        const{movies} = this.state;
-        const movieId = movies.indexOf(movie);
-        if(movies[movieId].star >=5)
-            return;
-        movies[movieId].star +=0.5;
-        this.setState({
-            //movies: movies
-            movies  //alternative or shortcut or above line for same movies
-        })
-    }
-
-    handleDecStar = (movie) =>{
-        const{movies} = this.state;
-        const movieId = movies.indexOf(movie);
-        if(movies[movieId].star <=0)
-            return;
-        movies[movieId].star -=0.5;
-        this.setState({
-            //movies: movies
-            movies  //alternative or shortcut or above line for same movies
-        })
-    }
-    handleFav = (movie) =>{
-        const{movies} = this.state;
-        const movieId = movies.indexOf(movie);
-        movies[movieId].fav = !movies[movieId].fav;
-        this.setState({
-            //movies: movies
-            movies  //alternative or shortcut or above line for same movies
-        })
-    }
-    handleCart = (movie) =>{
-        const{movies} = this.state;
-        const movieId = movies.indexOf(movie);
-        movies[movieId].isInCart = !movies[movieId].isInCart;
-        this.setState({
-            //movies: movies
-            movies  //alternative or shortcut or above line for same movies
-        })
-    }
-
 
     render(){
         
         //const{title,plot,price,rating,stars,fav,isInCart} = this.state;
-        const { movies } = this.state;
+        const { movies, addStars, removeStars, toggleCart, toggleFav } = this.props;
 
         return (
             <>
@@ -85,12 +25,11 @@ class MoiveList extends Component{
                 movies.map((movie) => (
                     <MoiveCard 
                         movies = {movie} 
-                        //key={index} 
                         key={movie.id} 
-                        addStars={this.handleIncStar} 
-                        removeStars={this.handleDecStar}
-                        handleFav={this.handleFav}
-                        handleCart={this.handleCart}
+                        addStars={addStars} 
+                        removeStars={removeStars}
+                        handleFav={toggleFav}
+                        handleCart={toggleCart}
                     />
                 )
                 )}
